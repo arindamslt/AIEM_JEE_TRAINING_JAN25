5ffcf6b939d3820c13b767ae8c477947410a2258
arindamslt <arin.saltlake@gmail.com>
2025-02-01 09:55:53 +0530

    JEE PROJECT

---
+++ JEE_PROJECT/FoodApp/src/main/java/master/servlet/FoodDelServe.java
@@ -0,0 +1,35 @@
+package master.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import master.dao.FoodDao;
+import master.dto.FoodDto;
+
+/**
+ * Servlet implementation class FoodDelServe
+ */
+@WebServlet("/FoodDelServe")
+public class FoodDelServe extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		String fid=request.getParameter("fid");
+		FoodDto fdto=new FoodDto();
+		fdto.setFid(fid);
+		FoodDao fdao=new FoodDao();
+		//fdao.addData(fdto);
+		fdao.deleteData(fdto);
+		response.sendRedirect("FoodList.jsp");
+	}
+
+}
